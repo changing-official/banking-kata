612d753c1a038fdd608c2f7296f2c02aa33d2bb9
Alison Henrique Jonck <alisonjonck@gmail.com>
2018-05-26 18:20:14 -0300

    adicionado deposit

--- account.py
+++ account.py
@@ -1,2 +1,16 @@
 class Account():
-    pass
+
+    money = 0
+
+    def deposit(self, value):
+        try:
+            convertido = float(value)
+
+            if(convertido < 0 ):
+                return False
+
+            self.money += convertido
+        except:
+            return False
+
+        return True
--- banking-kata-test.py
+++ banking-kata-test.py
@@ -4,7 +4,22 @@ from account import Account
 class TestBankingKata(unittest.TestCase):
 
     def testAccountExists(self):
-        self.assertIsNotNone(Account)
+        self.assertIsNotNone(Account())
+
+    def testAccountDepositAcceptsValue(self):
+        self.assertTrue(Account().deposit(100))
+
+    def testAccountDepositDontAcceptInvalidValue(self):
+        self.assertFalse(Account().deposit("100aa"))
+        
+        self.assertFalse(Account().deposit(-9))
+
+    def testAccountDepositShowsMoney(self):
+        conta = Account()
+        conta.deposit(100)
+        self.assertEqual(conta.money, 100)
+        conta.deposit(200)
+        self.assertEqual(conta.money, 300)
 
 if __name__ == '__main__':
     unittest.main()
